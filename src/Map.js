272a33fff12b881a0bc2e3f88638dca1b86ea6c0
RageOfFire <buihongson453@gmail.com>
2022-03-22 14:24:36 +0700

    not try those http

--- src/Map.js
+++ src/Map.js
@@ -1,7 +1,7 @@
 function Map() {
     return (
         <div>
-            <iframe class="w-full aspect-square" src="https://www.google.com/search?q=%http://mc.ragefiresmp.ga:4534/" title="Map"></iframe>
+            <iframe class="w-full aspect-square" src="http://mc.ragefiresmp.ga:4534/" title="Map"></iframe>
         </div>
     )
 }
